c9d44319722256f945e1d2f6b07167b85d8222b8
floraMoonDevWorld <induchowdary.fdu@gmail.com>
2020-07-07 11:38:24 -0400

    using hooks useState

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,78 +1,41 @@
-import React, {Component, useState} from 'react';
+import React, {Component, useState, useEffect} from 'react';
 import Order from "./Order"
 import {Button} from "react-bootstrap";
 import menuList1 from "../MenuList.js"
 
-class Menu extends Component{
+ export default function Menu() {
+   
 
-    
-    constructor() {
-        super();
-    this.state = {
-        menuList : menuList1 ,
-        
-        compare: {
-            arr: []
-        }
-    }
-        this.handleClick = this.handleClick.bind(this);
+let arr ={arr: []};
+    const [menuList, setMenuList] = useState(menuList1);
+    const [compare, setCompare] = useState(arr);
 
 
-    }
-
-    handleClick(e) {
-        let arrnew = this.state.compare.arr;
-        let menu = this.state.menuList;
-        let index1 = menu.findIndex((x) => 
-        //x.id === parseInt(e.target.id)
-        console.log(x.id)
-        );
-        let index = menu.findIndex((x) => x.id === parseInt(e.target.id));
-
-        // // if (index !== -1) {
-        //     // arrnew.splice(index, 1);
-        //   const menuObj = menuList[parseInt(e.target.id)];
-        //   menuObj.count = menuObj.count + 1;
-        //   menuObj.count = menuObj.count + 1;
-        //  this.setState({menuList,[parseInt(e.target.id)]: menuObj});
-        if(arrnew.length === 0)
-        arrnew.push(parseInt(e.target.id));
-            else if(arrnew.findIndex((x) => x===parseInt(e.target.id)) === -1)
+   function handleClick(e) {
+        let arrnew = compare.arr;
+        if(arrnew.length === 0 || arrnew.findIndex((x) => x===parseInt(e.target.id)) === -1)
             arrnew.push(parseInt(e.target.id));
 
-let tempMenu = [...this.state.menuList];
-let tempIte = {...tempMenu[parseInt(e.target.id)]};
-tempIte.count++;
-tempMenu[parseInt(e.target.id)]= tempIte;
-this.setState({menuList: tempMenu});
-            // compare
-        // }
-        // else {
-        //     if (arrnew.includes(index) === false)
-        //     {
-        //         arrnew.push(e.target.id)
-        //     }
 
-        // }
-        // console.log(event)
-        this.setState({ compare: { arr: arrnew } });
-
-        console.log(this.state.compare.arr);
-        let output = Object.keys(this.state.menuList).map(
-            i => this.state.menuList[i]
-        );
+        let tempMenu = [...menuList];
+        let tempIte = {...tempMenu[parseInt(e.target.id)]};
+        tempIte.count++;
+        tempMenu[parseInt(e.target.id)]= tempIte;
+        let compTemp = { arr: arrnew };
+        setMenuList(tempMenu);
+        setCompare(compTemp);
 
     }
 
 
-    render() {
+    // render() {
 
         return(
             <div className="container-fluid">
                <div className="row">
                <div className="col-md-8">
                     <div className="row">
-                        {this.state.menuList.map((menuList, index) => (
+                        {menuList.map((menuList, index) => (
                         <div className="col-md-4" key={menuList.id}>
                             <div className="card mb-3" style={{"max-width": "540px"}}>
                                 <div className="row no-gutters">
@@ -84,7 +47,7 @@ this.setState({menuList: tempMenu});
                                             <h5 className="card-title">{menuList.name}</h5>
                                             <p className="card-text">{menuList.desc}</p>
                                             <p className="card-text">
-                                                <Button className="bg-primary" style={{"color": "white"}} onClick={this.handleClick} id={menuList.id}>{menuList.price}</Button>
+                                                <Button className="bg-primary" style={{"color": "white"}} onClick={handleClick} id={menuList.id}>{menuList.price}</Button>
                                             </p>
                                         </div>
                                     </div>
@@ -94,11 +57,11 @@ this.setState({menuList: tempMenu});
                             ))}
                     </div>
                 </div>
-                  <HideShowOrderDisplay isShow= {this.state.compare.arr && this.state.compare.arr.length >0} compArr={this.state.compare.arr} menuList1 = {this.state.menuList}/>
+                  <HideShowOrderDisplay isShow= {compare.arr && compare.arr.length >0} compArr={compare.arr} menuList1 = {menuList}/>
                 </div>
             </div>
         )
-    }
+    // }
 }
 
 function HideShowOrderDisplay(props){
@@ -114,4 +77,4 @@ function HideShowOrderDisplay(props){
     })} </div>;
 }
 
-export default Menu;
+// export default Menu;
